d315e2f6c2f7d52b69e6194fee57550fe1051956
Ellen Lee <leechinghui.tw@gmail.com>
2017-11-24 01:37:50 +0800

    when click tab, user can view restaurants of certain category
    before_action :authenticate_user!

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,15 @@
 class CategoriesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_category
+
+  def show
+    @categories = Category.all
+    @restaurants = @category.restaurants.page(params[:page]).per(10)
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 end
