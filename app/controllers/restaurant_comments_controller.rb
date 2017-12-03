1af8d1cd94a6c69ecd83887c2d304e641f14d77b
pochia <lupochia@gmail.com>
2017-12-03 09:40:06 +0800

    add  restaurant create function

--- app/controllers/restaurant_comments_controller.rb
+++ app/controllers/restaurant_comments_controller.rb
@@ -1,2 +1,21 @@
 class RestaurantCommentsController < ApplicationController
+  before_action :set_restaurant
+
+  def create
+    @comment = @restaurant.comments.create(comment_params)
+    @comment.user = current_user
+    @comment.save!
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def comment_params
+    params[:comment].permit(:restaurant_id, :user_id, :comment_id, :comment)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+  
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,5 @@ class Restaurant < ApplicationRecord
 
   belongs_to :category
 
+  has_many :comments
 end
