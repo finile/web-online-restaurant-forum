c7064a084e4b3bbefbbf024fdb04f6d724cb4776
pochia <lupochia@gmail.com>
2017-12-03 08:58:53 +0800

    create comment model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130134427) do
+ActiveRecord::Schema.define(version: 20171203005327) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
