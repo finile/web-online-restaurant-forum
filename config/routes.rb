e2259351d59b6c9ee7dcbe2b5e28103682bdaeb1
pochia <lupochia@gmail.com>
2017-12-03 09:04:34 +0800

    change routes for comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :restaurants, only: [:index, :show]
-  resources :categories, only: :show
+  resources :restaurants, only: [:index, :show] do
+    resources :comments, controller: "restaurant_comments"
+  end
+
   root "restaurants#index"
-  
+  resources :categories, only: :show
+
   namespace :admin do
     resources :restaurants
     resources :categories
